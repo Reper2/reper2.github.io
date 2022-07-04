65f2e718b7b4214f61ce0216b82a85e894548eb9
Reper2 <87633097+Reper2@users.noreply.github.com>
2022-07-04 19:03:16 +1000

    large update

---
+++ scripts/music.js
@@ -0,0 +1,21 @@
+const
+    music = document.getElementById('music'),
+	musicSrc = document.createElement('source');
+music.controls = 'true';
+music.autoplay = 'true';
+music.loop = 'true';
+music.preload = 'auto';
+music.style.display = 'none';
+if (music.paused) {
+    music.play();
+};
+musicSrc.type = 'audio/mpeg';
+
+var
+    num = Math.floor(Math.random() * 53),
+    link = `/assets/music/${String(num).padStart(2, '0')}.mp3`;
+$(document).ready(function () {
+    musicSrc.src = link;
+    music.appendChild(musicSrc);
+    console.log('Added music to page.');
+});
