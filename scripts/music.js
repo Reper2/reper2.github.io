1704b6cdd5286cf43291a2de18b5ae7536abc3a4
Reper2 <87633097+Reper2@users.noreply.github.com>
2022-07-04 21:12:32 +1000

    cannot return 0 anymore

--- scripts/music.js
+++ scripts/music.js
@@ -14,8 +14,13 @@ musicSrc.type = 'audio/mpeg';
 var
     num = Math.floor(Math.random() * 50),
     link = `/assets/music/${String(num).padStart(2, '0')}.mp3`;
-$(document).ready(function () {
-    musicSrc.src = link;
-    music.appendChild(musicSrc);
-    console.log('Added music to page.');
-});
+if (num == 0) {
+    console.log('Returned 0, reloading page...');
+    window.location.reload;
+} else {
+    $(document).ready(function () {
+        musicSrc.src = link;
+        music.appendChild(musicSrc);
+        console.log('Added music to page.');
+    })
+};
