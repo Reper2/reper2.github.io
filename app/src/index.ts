c47d77a245fc19fae0e16f9381483469a7d099f8
Reper2 <ethan.reper@gmail.com>
2026-05-21 21:26:35 +1000

    Performance and loading improvements

---
+++ app/src/index.ts
@@ -0,0 +1,8 @@
+import './sw-installer.js';
+import './keybinds.js';
+import './app.js';
+import './eggs.js';
+import './debug-secret.js';
+import './audctrls.js';
+import './copyLinkBtn.js';
+import './footer.js';
